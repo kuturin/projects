52f5ed255a52eb719bb71d8f05d8134d32f4351e
kuturin <turkurun2@gmail.com>
2025-10-07 09:18:09 +0200

    feat: add unit variables for length, weight, and temperature to scripts.js in Unit Converter project

---
+++ Unit Converter/scripts.js
@@ -0,0 +1,23 @@
+let input;
+let lengthUnit;
+let milimetter = "mm";
+let centimetter = "cm";
+let metter = "m";
+let kilometer = "km";
+let inch = "in";
+let foot = "ft";
+let yard = "yd";
+let mile = "mi";
+
+let weightUnit;
+let miligram = "mg";
+let gram = "g";
+let kilogram = "kg";
+let ounce = "oz";
+let pound = "lb";
+
+let tempUnit;
+let celsius = "C";
+let fahrenheit = "F";
+let kelvin = "K";
+
