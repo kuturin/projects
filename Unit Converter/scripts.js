df55a38108900105d80c630986db8808077e19b0
kuturin <turkurun2@gmail.com>
2025-10-07 10:01:09 +0200

    feat: add complete HTML structure and unit variables for length, weight, and temperature converters in Unit Converter project

--- Unit Converter/scripts.js
+++ Unit Converter/scripts.js
@@ -1,6 +1,6 @@
-let input;
-let unitInput;
-let lengthUnit;
+let lengthUnit = document.getElementById("lengthUnit");
+let unitLengthInput = document.getElementById("unitLengthInput");
+let unitLengthOutput = document.getElementById("unitLengthOutput");
 let milimetter = "mm";
 let centimetter = "cm";
 let metter = "m";
@@ -10,15 +10,20 @@ let foot = "ft";
 let yard = "yd";
 let mile = "mi";
 
-let weightUnit;
+let weightUnit = document.getElementById("weightUnit");
+let unitWeightInput = document.getElementById("unitWeightInput");
+let unitWeightOutput = document.getElementById("unitWeightOutput");
 let miligram = "mg";
 let gram = "g";
 let kilogram = "kg";
 let ounce = "oz";
 let pound = "lb";
 
-let tempUnit;
+let tempUnit = document.getElementById("tempUnit")  ;
+let unitTempInput = document.getElementById("unitTempInput");
+let unitTempOutput = document.getElementById("unitTempOutput");
 let celsius = "C";
 let fahrenheit = "F";
 let kelvin = "K";
+
 
